b4cec05ee4c9f493ea285f96afc02caa4c9aa461
Azrofyi <azrofyi@yandex.ru>
2022-12-19 11:39:35 +0300

    game-prime

--- bin/game-prime.js
+++ bin/game-prime.js
@@ -4,25 +4,38 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Games!');
 const name = readlineSync.question('May i have your name? ');
 console.log(`Nice to meet you, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 const questions = () => {
   for (let i = 0; i < 3; ++i) {
     const randomNum = Math.floor(Math.random() * 100);
     console.log(`Question: ${randomNum}`);
     const answer = readlineSync.question('Your answer? ');
-    const isEven = randomNum % 2 === 0;
+    let isPrime = true;
     let result;
 
-    if (randomNum % 2 === 0) {
+    if (randomNum === 1) {
+      isPrime = false;
+    } else if (randomNum > 1) {
+      for (let n = 2; n < randomNum; ++n) {
+        if (randomNum % n === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+    } else {
+      isPrime = true;
+    }
+
+    if (isPrime === true) {
       result = 'yes';
     } else {
       result = 'no';
     }
 
-    if (answer === 'yes' && isEven === true) {
+    if (answer === 'yes' && isPrime === true) {
       console.log('Correct!');
-    } else if (answer === 'no' && isEven === false) {
+    } else if (answer === 'no' && isPrime === false) {
       console.log('Correct!');
     } else {
       return console.log(
